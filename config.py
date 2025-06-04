2fd648b4cba6ebfcd5c06a17c388119a547e2591
wakil86 <139179434+wakil86@users.noreply.github.com>
2025-06-04 14:58:36 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 class Config:
-<<<<<<< HEAD
-    SQLALCHEMY_DATABASE_URI = "postgresql://flaskuser:flaskuser1234@db:5432/flask_jwt_db"
-=======
-    SQLALCHEMY_DATABASE_URI = "postgresql://poridhi:poridhi1234@db:5432/flask_jwt_db"
->>>>>>> b4c068ab1ec9864399f65ed7d5c8f3135b78a87a
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "postgresql://poridhi:poridhi1234@localhost:5432/flask_jwt_db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "supersecretkey"
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "supersecretkey")
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access']
